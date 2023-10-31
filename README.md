0a71012dee2b3161febc5b04e3ba7165e7842d38
TomokiIkegami <gampsocleis.ussuriensis@gmail.com>
2023-10-31 09:03:39 +0900

    READMEにInstallationを追加

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ This is a GUI program for the PC to conrtrol Elefit.
 ## elefit_for_arduino_nano
 This is a program for arduino.
 This file contains arduino sketch and GUI program.
+
+# Installation
+1. Download and install software 「[Processing](https://processing.org/)」 and 「[Arduino IDE(https://www.arduino.cc/en/software)」.
+2. Open Arduino IDE.
+2. Connect your PC and Elefit using USB (Micro-B) cable.
+3. Select board and COM port. (Board name: Arduino nano)
+4. Upload sketch (Sketch file name: elefit_for_arduino_nano.ino).
+5. Open Processing.
+6. Open sketch (Sketch file name: elefit_arduino_nano_gui.pde).
+7. Install library 「controlP5」.
+8. Press a run botton and open GUI.
--- for_moblile_version/elefit_for_arduino_nano_mobile/elefit_for_arduino_nano_mobile.ino
+++ for_moblile_version/elefit_for_arduino_nano_mobile/elefit_for_arduino_nano_mobile.ino
@@ -1,28 +1,28 @@
-/* Bluetooth通信に必要 */
-#include "BluetoothSerial.h"  //ArduinoのBluetooth通信に使用
-BluetoothSerial BTS;  //BTSという名前でオブジェクトを定義
-
-/* 制御命令保存用の変数 */
-String input = "1000;";
-
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(115200);               //シリアルモニタで確認用。伝送速度を115200[bps]に設定
-  BTS.begin("Elefit");  //接続画面で表示される名前を設定 ★好きな名前にしてよい
-
-}
-
-// the loop function runs over and over again forever
-void loop() {
-  if (BTS.available()) {               //available()で受信した文字があるか確認。あればif文内の処理を実行。
-    t1 = millis();                        //データを取得した時間を記録
-    input = BTS.readStringUntil(';');  //文字をセミコロン(;)まで読んで、文字列として変数inputに保存。
-    delay_time = input.toInt();  //入力値を整数に変換
-  }
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(delay_time);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(delay_time);                       // wait for a second
-}
+///* Bluetooth通信に必要 */
+//#include "BluetoothSerial.h"  //ArduinoのBluetooth通信に使用
+//BluetoothSerial BTS;  //BTSという名前でオブジェクトを定義
+//
+///* 制御命令保存用の変数 */
+//String input = "1000;";
+//
+//// the setup function runs once when you press reset or power the board
+//void setup() {
+//  // initialize digital pin LED_BUILTIN as an output.
+//  pinMode(LED_BUILTIN, OUTPUT);
+//  Serial.begin(115200);               //シリアルモニタで確認用。伝送速度を115200[bps]に設定
+//  BTS.begin("Elefit");  //接続画面で表示される名前を設定 ★好きな名前にしてよい
+//
+//}
+//
+//// the loop function runs over and over again forever
+//void loop() {
+//  if (BTS.available()) {               //available()で受信した文字があるか確認。あればif文内の処理を実行。
+//    t1 = millis();                        //データを取得した時間を記録
+//    input = BTS.readStringUntil(';');  //文字をセミコロン(;)まで読んで、文字列として変数inputに保存。
+//    delay_time = input.toInt();  //入力値を整数に変換
+//  }
+//  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+//  delay(delay_time);                       // wait for a second
+//  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+//  delay(delay_time);                       // wait for a second
+//}
