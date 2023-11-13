3a31146a8976cb0d9513bc991ebd504f94b9c9aa
TomokiIkegami <gampsocleis.ussuriensis@gmail.com>
2023-11-13 09:29:45 +0900

    マイコン側のプログラムはスマホ版でも変更なしです

--- for_moblile_version/elefit_for_arduino_nano_mobile/elefit_for_arduino_nano_mobile.ino
+++ for_moblile_version/elefit_for_arduino_nano_mobile/elefit_for_arduino_nano_mobile.ino
@@ -1,29 +1,213 @@
-/*ハードウェアの接続ピンの設定*/
-//#define LED_PIN 2 //走行には無関係。2番ピンにLEDのアノード(+)を接続すると割り込み処理の間隔(100ms)でLEDが点滅
+#define PWM_PORT_M1_1 2  //DPE-100-7P-Y1 (A)
+#define PWM_PORT_M2_1 3  //DPE-100-7P-Y1 (B)
+#define PWM_PORT_M3_1 4  //DPE-100-7P-Y1 (C)
 
-String input=""; // スマホアプリの信号を保存しておく変数
+#define PWM_PORT_M4_1 9  //6-channel pump (R+)
+#define PWM_PORT_M4_2 10  //6-channel pump (R-)
 
-void setup(){
-  Serial.begin(115200); //シリアル通信の伝送速度を1115200[bps]に設定
-  pinMode(LED_BUILTIN, OUTPUT); //LEDの点灯ピンを出力用に設定
+#define PWM_PORT_M5_1 11  //6-channel pump (L+)
+#define PWM_PORT_M5_2 12  //6-channel pump (L-)
+
+#define LIMIT_SWITCH  A0   //Limit switch
+
+#define STEP_PORT_1   5  //PFL20-24Q A
+#define STEP_PORT_2   6  //PFL20-24Q A'
+#define STEP_PORT_3   7  //PFL20-24Q B
+#define STEP_PORT_4   8  //PFL20-24Q B'
+
+#define DELAYTIME     4 //stepermotomer sleeptime
+
+#define ELEMENTS_NUM 3 /**< カンマ区切りデータの項目数 */
+
+int PWM_RANGE     = 100; //Maximum value of generated PWM(1~100)
+int FREQ          = 100; //pwm frequency (Hz)
+int reverse = 0;         //12chpump reverse func
+int speed1 = 100;       //12chpump(R) revsepeed
+int speed2 = 100;       //12chpump(L) revsepeed
+
+static String elements[ELEMENTS_NUM];
+static int received_elements_num = 0; /**< 受信済み文字列の数 */
+
+void setup() {
+  Serial.begin(9600);
+  /***** GPIO pin setup *****/
+  pinMode(PWM_PORT_M1_1, OUTPUT);     //DPE-100-7P-Y1 (A)
+  pinMode(PWM_PORT_M2_1, OUTPUT);     //DPE-100-7P-Y1 (B)
+  pinMode(PWM_PORT_M3_1, OUTPUT);     //DPE-100-7P-Y1 (C)
+  pinMode(PWM_PORT_M4_1, OUTPUT);     //6-channel pump (R+)
+  pinMode(PWM_PORT_M4_2, OUTPUT);     //6-channel pump (R-)
+  pinMode(PWM_PORT_M5_1, OUTPUT);     //6-channel pump (L+)
+  pinMode(PWM_PORT_M5_2, OUTPUT);     //6-channel pump (L-)
+
+  pinMode(STEP_PORT_1,OUTPUT);        //PFL20-24Q A
+  pinMode(STEP_PORT_2,OUTPUT);        //PFL20-24Q A'
+  pinMode(STEP_PORT_3,OUTPUT);        //PFL20-24Q B
+  pinMode(STEP_PORT_4,OUTPUT);        //PFL20-24Q B'
+
+  pinMode(LIMIT_SWITCH,INPUT_PULLUP); //Limit switch
+
+  /***** GPIO initial setup *****/
+  digitalWrite(PWM_PORT_M1_1, LOW);
+  digitalWrite(PWM_PORT_M2_1, LOW);
+  digitalWrite(PWM_PORT_M3_1, LOW);
+  digitalWrite(PWM_PORT_M4_1, LOW);
+  digitalWrite(PWM_PORT_M4_2, LOW);
+  digitalWrite(PWM_PORT_M5_1, LOW);
+  digitalWrite(PWM_PORT_M5_2, LOW);
+  digitalWrite(STEP_PORT_1, LOW);
+  digitalWrite(STEP_PORT_2, LOW);
+  digitalWrite(STEP_PORT_3, LOW);
+  digitalWrite(STEP_PORT_4, LOW);
 }
 
-void loop(){
 
-  if(Serial.available()>0){
-    input = Serial.readStringUntil(';');  //文字をセミコロン(;)まで読んで、文字列として変数inputに保存。
+void(*resetFunc)(void) = 0; // Arduinoをリセットボタンでなく、プログラムからリセットするための関数
+
+void loop() {
+  if (Serial.available()) {
+    String line;              // 受信文字列
+    unsigned int beginIndex;  // 要素の開始位置
+
+    // シリアルモニタやProcessingから"AB,C,DEF,12,3,45,A1,2B,-1,+127"のように
+    // ELEMENTS_NUM個の文字列の間にカンマを付けて送る
+    // 送信側の改行設定は「LFのみ」にすること
+    // シリアル通信で1行（改行コードまで）読み込む
+    line = Serial.readStringUntil('\n');
+
+    beginIndex = 0;
+    for (received_elements_num = 0; received_elements_num < ELEMENTS_NUM; received_elements_num++) {
+        // 最後の要素ではない場合
+        if (received_elements_num != (ELEMENTS_NUM - 1)) {
+            // 要素の開始位置から，カンマの位置を検索する
+            unsigned int endIndex;
+            endIndex = line.indexOf(',', beginIndex);
+            // カンマが見つかった場合
+            if (endIndex != -1) {
+                // 文字列を切り出して配列に格納する
+                elements[received_elements_num] = line.substring(beginIndex, endIndex);
+                // 要素の開始位置を更新する
+                beginIndex = endIndex + 1;
+            }
+            // カンマが見つからなかった場合はfor文を中断する
+            else {
+                break;
+            }
+        }
+        // 最後の要素の場合
+        else {
+            elements[received_elements_num] = line.substring(beginIndex);
+        }
+    }
+    if(elements[0] == "step_front"){ // ステッピングモータを駆動してカラムを前方に動かす
+      Serial.println(elements[0]);
+      step_front(elements[1].toInt());
+    }
+    else if(elements[0] == "step_back"){  // ステッピングモータを駆動してカラムを後方に動かす
+      Serial.println(elements[0]);
+      step_back(elements[1].toInt());   
+    }
+    else if(elements[0] == "on_pump_12ch"){  // 6chポンプ×2を駆動
+      Serial.println(elements[0]);
+      Serial.println(elements[1]);
+      Serial.println(elements[2]);
+      on_pump_12ch(elements[1].toInt(),elements[2].toInt()); 
+    }
+    else if(elements[0] == "pwm_pump_12ch"){  // 6ch×2のポンプをわずかに正転させ、停止する
+      Serial.println(elements[0]);
+      pwm_pump_12ch(); 
+    }
+    else if(elements[0] == "off_pump_12ch"){  // 6chポンプ×2を停止
+      Serial.println(elements[0]);
+      off_pump_12ch(); 
+    }
+    else if(elements[0] == "on_pump_dba"){  // ダイヤフラムポンプを駆動
+      Serial.println(elements[0]);
+      if(elements[1].toInt() == 1){
+        digitalWrite(PWM_PORT_M1_1, HIGH);
+      }
+      else if(elements[1].toInt() == 2){
+        digitalWrite(PWM_PORT_M2_1, HIGH);
+      }
+      else if(elements[1].toInt() == 3){
+        digitalWrite(PWM_PORT_M3_1, HIGH);
+      }
+    }
+    else if(elements[0] == "off_pump_dba"){ // ダイヤフラムポンプを停止
+      Serial.println(elements[0]);
+      digitalWrite(PWM_PORT_M1_1, LOW);
+      digitalWrite(PWM_PORT_M2_1, LOW);
+      digitalWrite(PWM_PORT_M3_1, LOW);
+    }else if(elements[0] == "reset_arduino"){ // Ardrinoをリセットする（動作はリセットボタンを押したときと同じ）
+      Serial.println(elements[0]);
+      delay(100);
+      resetFunc();
+    }
+    elements[0] == "\0";    // 文字列の初期化
   }
-  /*受信した信号を表示*/
-  Serial.print("input=");
-  Serial.print(input);
-  Serial.println();
+}
+/* ステッピングモータを駆動してカラムを前方に動かす関数 */
+int step_front(int step){
+  digitalWrite(STEP_PORT_2, HIGH);
+  digitalWrite(STEP_PORT_4, HIGH);
 
-  /*入力した文字列に応じて動作を設定*/
-  if(input=="b"){
-    digitalWrite(LED_BUILTIN, HIGH);  //内臓LEDを点灯
-  }else{
-    digitalWrite(LED_BUILTIN, LOW);  //内臓LEDを消灯
+  for(int i=0; i < step; i++){
+    digitalWrite(STEP_PORT_1, HIGH);
+    delay(DELAYTIME);
+    digitalWrite(STEP_PORT_3, HIGH);
+    delay(DELAYTIME);
+    digitalWrite(STEP_PORT_1, LOW);
+    delay(DELAYTIME);
+    digitalWrite(STEP_PORT_3, LOW);
+    delay(DELAYTIME);
   }
-  delay(10);  //信号を確実に受信できるようにするための待ち
+  digitalWrite(STEP_PORT_2, LOW);
+  digitalWrite(STEP_PORT_4, LOW);
+}
+/* ステッピングモータを駆動してカラムを後方に動かす関数 */
+int step_back(int step){
+  digitalWrite(STEP_PORT_2, HIGH);
+  digitalWrite(STEP_PORT_4, HIGH);
 
+  for(int i=0; i < step; i++){
+
+    if(digitalRead(LIMIT_SWITCH) == HIGH){
+      break;
+    }
+    digitalWrite(STEP_PORT_3, HIGH);
+    delay(DELAYTIME);
+    digitalWrite(STEP_PORT_1, HIGH);
+    delay(DELAYTIME);
+    digitalWrite(STEP_PORT_3, LOW);
+    delay(DELAYTIME);
+    digitalWrite(STEP_PORT_1, LOW);
+    delay(DELAYTIME);
+  }
+  digitalWrite(STEP_PORT_2, LOW);
+  digitalWrite(STEP_PORT_4, LOW);
+}
+/* 6ch×2のポンプ正転・逆転する関数 */
+int on_pump_12ch(int speed,bool rev){
+  if(rev == 0){
+    analogWrite(PWM_PORT_M4_1, speed1 * 2.55);
+    analogWrite(PWM_PORT_M5_2, speed2 * 2.55);
+  }
+  else if(rev == 1){
+    analogWrite(PWM_PORT_M4_2, speed1 * 2.55);
+    analogWrite(PWM_PORT_M5_1, speed2 * 2.55);
+    
+  }
+}
+/* 6ch×2のポンプ停止する関数 */
+void off_pump_12ch(){
+  digitalWrite(PWM_PORT_M4_1, LOW);
+  digitalWrite(PWM_PORT_M4_2, LOW);
+  digitalWrite(PWM_PORT_M5_1, LOW);
+  digitalWrite(PWM_PORT_M5_2, LOW);
+}
+/* 6ch×2のポンプをわずかに正転させ、停止する関数 */
+void pwm_pump_12ch(){
+  on_pump_12ch(80,0);
+  delay(385);
+  off_pump_12ch();
+  delay(625);
 }
