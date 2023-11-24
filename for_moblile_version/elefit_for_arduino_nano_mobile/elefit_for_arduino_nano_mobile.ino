646b2e2238e694ef34ad30e6b8d7c3f167820137
TomokiIkegami <gampsocleis.ussuriensis@gmail.com>
2023-11-24 09:28:34 +0900

    arduino側のプログラムで時間を制御することにしました

--- for_moblile_version/elefit_for_arduino_nano_mobile/elefit_for_arduino_nano_mobile.ino
+++ for_moblile_version/elefit_for_arduino_nano_mobile/elefit_for_arduino_nano_mobile.ino
@@ -1,3 +1,5 @@
+#include <FlexiTimer2.h>
+
 #define PWM_PORT_M1_1 2  //DPE-100-7P-Y1 (A)
 #define PWM_PORT_M2_1 3  //DPE-100-7P-Y1 (B)
 #define PWM_PORT_M3_1 4  //DPE-100-7P-Y1 (C)
@@ -28,8 +30,68 @@ int speed2 = 100;       //12chpump(L) revsepeed
 static String elements[ELEMENTS_NUM];
 static int received_elements_num = 0; /**< 受信済み文字列の数 */
 
+boolean running_flag = false;   // 処理の実行管理用フラグ
+
+// シリアル通信でスマホからの命令を読み込む関数
+void read_data(){
+    if (Serial.available()) {
+        String line;              // 受信文字列
+        unsigned int beginIndex;  // 要素の開始位置
+
+    
+        // シリアルモニタやProcessingから"AB,C,DEF,12,3,45,A1,2B,-1,+127"のように
+        // ELEMENTS_NUM個の文字列の間にカンマを付けて送る
+        // 送信側の改行設定は「LFのみ」にすること
+        // シリアル通信で1行（改行コードまで）読み込む
+        line = Serial.readStringUntil('\n');
+
+        // reset_arduinoが来たら、ほかの処理中でも処理を中断
+        if((running_flag == true) && (line == "reset_arduino,0,0\n")){
+          exit(0);  // 強制終了
+          } 
+
+
+        
+        beginIndex = 0;
+        for (received_elements_num = 0; received_elements_num < ELEMENTS_NUM; received_elements_num++) {
+            // 最後の要素ではない場合
+            if (received_elements_num != (ELEMENTS_NUM - 1)) {
+                // 要素の開始位置から，カンマの位置を検索する
+                unsigned int endIndex;
+                endIndex = line.indexOf(',', beginIndex);
+                // カンマが見つかった場合
+                if (endIndex != -1) {
+                    // 文字列を切り出して配列に格納する
+                    elements[received_elements_num] = line.substring(beginIndex, endIndex);
+                    // 要素の開始位置を更新する
+                    beginIndex = endIndex + 1;
+                }
+                // カンマが見つからなかった場合はfor文を中断する
+                else {
+                    break;
+                }
+            }
+            // 最後の要素の場合
+            else {
+                elements[received_elements_num] = line.substring(beginIndex);
+            }
+        }
+//      elements[0] == "\0";    // 文字列の初期化
+      if(running_flag){ // 実行中(running_flag==ture)なら他の処理を実行しない
+          elements[0]="running_another_process";
+          }
+      Serial.end();
+      Serial.begin(115200);
+  }
+  
+  }
+
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200);             // シリアル伝送速度を115200[bps]に設定（Bluno Nano標準）
+  FlexiTimer2::set(500,read_data);  // 500[ms]間隔で命令を読むようにタイマーを設定
+  FlexiTimer2::start();             // タイマー開始
+  
   /***** GPIO pin setup *****/
   pinMode(PWM_PORT_M1_1, OUTPUT);     //DPE-100-7P-Y1 (A)
   pinMode(PWM_PORT_M2_1, OUTPUT);     //DPE-100-7P-Y1 (B)
@@ -65,40 +127,40 @@ void(*resetFunc)(void) = 0; // Arduinoをリセットボタンでなく、プロ
 
 void loop() {
   
-  if (Serial.available()) {
-//    Serial.flush();
-    String line;              // 受信文字列
-    unsigned int beginIndex;  // 要素の開始位置
-
-    // シリアルモニタやProcessingから"AB,C,DEF,12,3,45,A1,2B,-1,+127"のように
-    // ELEMENTS_NUM個の文字列の間にカンマを付けて送る
-    // 送信側の改行設定は「LFのみ」にすること
-    // シリアル通信で1行（改行コードまで）読み込む
-    line = Serial.readStringUntil('\n');
-    beginIndex = 0;
-    for (received_elements_num = 0; received_elements_num < ELEMENTS_NUM; received_elements_num++) {
-        // 最後の要素ではない場合
-        if (received_elements_num != (ELEMENTS_NUM - 1)) {
-            // 要素の開始位置から，カンマの位置を検索する
-            unsigned int endIndex;
-            endIndex = line.indexOf(',', beginIndex);
-            // カンマが見つかった場合
-            if (endIndex != -1) {
-                // 文字列を切り出して配列に格納する
-                elements[received_elements_num] = line.substring(beginIndex, endIndex);
-                // 要素の開始位置を更新する
-                beginIndex = endIndex + 1;
-            }
-            // カンマが見つからなかった場合はfor文を中断する
-            else {
-                break;
-            }
-        }
-        // 最後の要素の場合
-        else {
-            elements[received_elements_num] = line.substring(beginIndex);
-        }
-    }
+//  if (Serial.available()) {
+//    String line;              // 受信文字列
+//    unsigned int beginIndex;  // 要素の開始位置
+//
+//    // シリアルモニタやProcessingから"AB,C,DEF,12,3,45,A1,2B,-1,+127"のように
+//    // ELEMENTS_NUM個の文字列の間にカンマを付けて送る
+//    // 送信側の改行設定は「LFのみ」にすること
+//    // シリアル通信で1行（改行コードまで）読み込む
+//    line = Serial.readStringUntil('\n');
+//    beginIndex = 0;
+//    for (received_elements_num = 0; received_elements_num < ELEMENTS_NUM; received_elements_num++) {
+//        // 最後の要素ではない場合
+//        if (received_elements_num != (ELEMENTS_NUM - 1)) {
+//            // 要素の開始位置から，カンマの位置を検索する
+//            unsigned int endIndex;
+//            endIndex = line.indexOf(',', beginIndex);
+//            // カンマが見つかった場合
+//            if (endIndex != -1) {
+//                // 文字列を切り出して配列に格納する
+//                elements[received_elements_num] = line.substring(beginIndex, endIndex);
+//                // 要素の開始位置を更新する
+//                beginIndex = endIndex + 1;
+//            }
+//            // カンマが見つからなかった場合はfor文を中断する
+//            else {
+//                break;
+//            }
+//        }
+//        // 最後の要素の場合
+//        else {
+//            elements[received_elements_num] = line.substring(beginIndex);
+//        }
+//    }
+    
     if(elements[0] == "step_front"){ // ステッピングモータを駆動してカラムを前方に動かす
       Serial.println(elements[0]);
       step_front(elements[1].toInt());
@@ -142,14 +204,17 @@ void loop() {
       Serial.println(elements[0]);
       delay(100);
       resetFunc();
-    }
-    elements[0] == "\0";    // 文字列の初期化
-    Serial.end();
-    Serial.begin(115200);
-  }
+    }else if(elements[0] == "running_another_process"){
+      // 他の処理を実行中は何もしない
+      }
+//    elements[0] == "\0";    // 文字列の初期化
+//    Serial.end();
+//    Serial.begin(115200);
+//  }
 }
 /* ステッピングモータを駆動してカラムを前方に動かす関数 */
 int step_front(int step){
+  running_flag = true; // 動作の実行フラグを立てる（tureの間は他の処理を実行しない）
   digitalWrite(STEP_PORT_2, HIGH);
   digitalWrite(STEP_PORT_4, HIGH);
 
@@ -165,9 +230,11 @@ int step_front(int step){
   }
   digitalWrite(STEP_PORT_2, LOW);
   digitalWrite(STEP_PORT_4, LOW);
+  running_flag = false; // 処理が完了したフラグを下ろす（フラグをfalseに）
 }
 /* ステッピングモータを駆動してカラムを後方に動かす関数 */
 int step_back(int step){
+  running_flag = true; // 動作の実行フラグを立てる（tureの間は他の処理を実行しない）
   digitalWrite(STEP_PORT_2, HIGH);
   digitalWrite(STEP_PORT_4, HIGH);
 
@@ -187,9 +254,11 @@ int step_back(int step){
   }
   digitalWrite(STEP_PORT_2, LOW);
   digitalWrite(STEP_PORT_4, LOW);
+  running_flag = false; // 処理が完了したフラグを下ろす（フラグをfalseに）
 }
 /* 6ch×2のポンプ正転・逆転する関数 */
 int on_pump_12ch(int speed,bool rev){
+  running_flag = true; // 動作の実行フラグを立てる（tureの間は他の処理を実行しない）
   if(rev == 0){
     analogWrite(PWM_PORT_M4_1, speed1 * 2.55);
     analogWrite(PWM_PORT_M5_2, speed2 * 2.55);
@@ -199,18 +268,23 @@ int on_pump_12ch(int speed,bool rev){
     analogWrite(PWM_PORT_M5_1, speed2 * 2.55);
     
   }
+  running_flag = false; // 処理が完了したフラグを下ろす（フラグをfalseに）
 }
 /* 6ch×2のポンプ停止する関数 */
 void off_pump_12ch(){
+  running_flag = true; // 動作の実行フラグを立てる（tureの間は他の処理を実行しない）
   digitalWrite(PWM_PORT_M4_1, LOW);
   digitalWrite(PWM_PORT_M4_2, LOW);
   digitalWrite(PWM_PORT_M5_1, LOW);
   digitalWrite(PWM_PORT_M5_2, LOW);
+  running_flag = false; // 処理が完了したフラグを下ろす（フラグをfalseに）
 }
 /* 6ch×2のポンプをわずかに正転させ、停止する関数 */
 void pwm_pump_12ch(){
+  running_flag = true; // 動作の実行フラグを立てる（tureの間は他の処理を実行しない）
   on_pump_12ch(80,0);
   delay(385);
   off_pump_12ch();
   delay(625);
+  running_flag = false; // 処理が完了したフラグを下ろす（フラグをfalseに）
 }
