bdb05ac1a561c08e9aa019a1fb1c55f9724965be
TomokiIkegami <gampsocleis.ussuriensis@gmail.com>
2023-12-25 09:29:40 +0900

    Arduinoコードに、プロセスの進捗表示用の処理を追加しました

--- for_moblile_version/elefit_for_arduino_nano_mobile/elefit_for_arduino_nano_mobile.ino
+++ for_moblile_version/elefit_for_arduino_nano_mobile/elefit_for_arduino_nano_mobile.ino
@@ -43,7 +43,7 @@ unsigned long t_11 = 18;
 unsigned long t_12 = 45;
 unsigned long t_13 = 60;  // Wait for 600 sec (test = 60 sec)
 unsigned long t_14 = 30;
-unsigned long t_1_error = 3 // ステッピングモータの動作などで発生する遅延
+unsigned long t_1_error = 3; // ステッピングモータの動作などで発生する遅延
 /* Phase2(Loading)、各処理の実行時間（t_21~t_26）[sec] */
 unsigned long t_21 = 19;
 unsigned long t_22 = 45;
@@ -51,7 +51,7 @@ unsigned long t_23 = 30;  // Wait for 300 sec (test = 30 sec)
 unsigned long t_24 = 25;  // Wait for 250 sec (test = 25 sec)
 unsigned long t_25 = 4;
 unsigned long t_26 = 60;
-unsigned long t_2_error = 0 // ステッピングモータの動作などで発生する遅延
+unsigned long t_2_error = 0; // ステッピングモータの動作などで発生する遅延
 /* Phase3(Collecting)、各処理の実行時間（t_31~t_36） [sec]*/
 unsigned long t_31 = 15;
 unsigned long t_32 = 45;
@@ -59,55 +59,80 @@ unsigned long t_33 = 5;
 unsigned long t_34 = 60;
 unsigned long t_35 = 5;
 unsigned long t_36 = 100;
-unsigned long t_3_error = 5 // ステッピングモータの動作などで発生する遅延
+unsigned long t_3_error = 5; // ステッピングモータの動作などで発生する遅延
+
 /* 進捗表示のゲージ描画用 */
 String excecuted_process = ""; // 実行されているプロセス名を保存
-unsigned long estimated_time = 0; // 動作にかかる時間の合計値（命令受信後に計算）
-// Phase1(Washing)の所要時間合計
-unsigned long washing_time_total    = t_11 + t_12 + t_13 + t_14 + t_1_error;
-// Phase2(Loading)の所要時間合計
-unsigned long loading_time_total    = t_21 + t_22 + t_23 + t_24 + (t_25 + t_26) * 3 + t_2_error;
-// Phase3(Collecting)の所要時間合計
-unsigned long collecting_time_total = t_32 + t_32 + t_33 + t_34 + t_35 + t_36 + t_3_error;
+unsigned long estimated_time_total = 0; // 動作にかかる時間の合計値（命令受信後に計算）
+unsigned long the_remaining_time = 0;   // 現在の残り時間（タイマー割り込みごとに更新される）
+// Phase1(Washing)の所要時間合計[msec]
+unsigned long washing_time_total    = (t_11 + t_12 + t_13 + t_14 + t_1_error) * 1000;
+// Phase2(Loading)の所要時間合計[msec]
+unsigned long loading_time_total    = (t_21 + t_22 + t_23 + t_24 + (t_25 + t_26) * 3 + t_2_error) * 1000;
+// Phase3(Collecting)の所要時間合計[msec]
+unsigned long collecting_time_total = (t_32 + t_32 + t_33 + t_34 + t_35 + t_36 + t_3_error) * 1000;
 // Phase1(Washing), Phase2(Loading), Phase3 (Collecting)が実行された時間[msec]
 unsigned long washing_time = 0; // Washing の実行時間[msec]
 unsigned long loading_time = 0; // Loading の実行時間[msec]
 unsigned long collecting_time = 0; // Collecting の実行時間[msec]
+// Phase1(Washing), Phase2(Loading), Phase3 (Collecting)の進捗
+float washing_progress = 0;
+float loading_progress = 0;
+float collecting_progress = 0;
 
 /* Arduinoリセット用関数の定義 */
 void(*resetFunc)(void) = 0; // Arduinoをリセットボタンでなく、プログラムからリセットするための関数
 
 // シリアル通信でスマホからの命令を読み込む関数
 void read_data() {
-    if (excecuted_process == "washing") { // Washingの処理を実行
-      washing_time += 500; // 実行時間を加算（500[msec]はタイマー割り込みの周期）
-    }
-    else if (excecuted_process == "loading") { // Loadingの処理を実行
-      loading_time += 500; // 実行時間を加算（500[msec]はタイマー割り込みの周期）
-    }
-    else if (excecuted_process == "collecting") { // Collectingの処理を実行
-      collecting_time += 500; // 実行時間を加算（500[msec]はタイマー割り込みの周期）
-    }
-    else if (excecuted_process == "close") {
-      washing_time = 0; // 実行時間をリセット
-      loading_time = 0; // 実行時間をリセット
-      loading_time = 0; // 実行時間をリセット
-    }
+  if (excecuted_process == "washing") { // Washingの処理を実行
+    washing_time += 500; // 実行時間を加算（500[msec]はタイマー割り込みの周期）
+  }
+  else if (excecuted_process == "loading") { // Loadingの処理を実行
+    loading_time += 500; // 実行時間を加算（500[msec]はタイマー割り込みの周期）
+  }
+  else if (excecuted_process == "collecting") { // Collectingの処理を実行
+    collecting_time += 500; // 実行時間を加算（500[msec]はタイマー割り込みの周期）
+  }
+  else if (excecuted_process == "close") {
+    washing_time = 0; // 実行時間をリセット
+    loading_time = 0; // 実行時間をリセット
+    loading_time = 0; // 実行時間をリセット
+  }
 
-  
+  //  the_remaining_time -= 500 ;
+  the_remaining_time = estimated_time_total - (washing_time + loading_time + collecting_time); //　残り時間を更新
+  if (the_remaining_time > 0) {
+    Serial.print("The remaining time[msec]:");
+    Serial.print(the_remaining_time);
+    Serial.println();
+    washing_progress = washing_time / washing_time_total;
+    loading_progress = loading_time / loading_time_total;
+    collecting_progress = collecting_time / collecting_time_total;
+    Serial.print("Progress[%]:");
+    Serial.print("Washing,");
+    Serial.print(washing_progress*100);
+    Serial.print(",Loading,");
+    Serial.print(loading_progress*100);
+    Serial.print(",Collecting,");
+    Serial.print(collecting_progress*100);
+    Serial.println();
+    Serial.println();
+  }
+
   if (Serial.available()) {
     rx_sig_count++;
     String line;              // 受信文字列
     unsigned int beginIndex;  // 要素の開始位置
-    Serial.print("rx_sig_count = ");
-    Serial.print(rx_sig_count);
-    Serial.print(", rx_sig_count_prev = ");
-    Serial.print(rx_sig_count_prev);
-    Serial.println();
+    //    Serial.print("rx_sig_count = ");
+    //    Serial.print(rx_sig_count);
+    //    Serial.print(", rx_sig_count_prev = ");
+    //    Serial.print(rx_sig_count_prev);
+    //    Serial.println();
 
     if (rx_sig_count >= rx_sig_count_prev + 2) {
-      Serial.print("*** Please wait. Excecuting the received command ... ***\n");
-      Serial.print("*** If you want to stop this process, Press the「CLOSE」button on your app. ***\n");
+      //      Serial.print("*** Please wait. Excecuting the received command ... ***\n");
+      //      Serial.print("*** If you want to stop this process, Press the「CLOSE」button on your app. ***\n");
     }
 
     // シリアルモニタやProcessingから"AB,C,DEF,12,3,45,A1,2B,-1,+127"のように
@@ -195,13 +220,13 @@ void loop() {
   delay(50);
 
   if (rx_sig_count != rx_sig_count_prev) {
-    Serial.print(">> Received command = ");
-    Serial.print(elements[0]);
-    Serial.print(",");
-    Serial.print(elements[1]);
-    Serial.print(",");
-    Serial.print(elements[2]);
-    Serial.println();
+    //    Serial.print(">> Received command = ");
+    //    Serial.print(elements[0]);
+    //    Serial.print(",");
+    //    Serial.print(elements[1]);
+    //    Serial.print(",");
+    //    Serial.print(elements[2]);
+    //    Serial.println();
     if (elements[0] == "step_front") { // ステッピングモータを駆動してカラムを前方に動かす
       step_front(elements[1].toInt());
     }
@@ -224,15 +249,20 @@ void loop() {
       off_pump_dba();
     }
     else if (elements[0] == "washing") { // Washingの処理を実行
+      estimated_time_total = washing_time_total; // 合計の所要時間を計算
+      the_remaining_time = estimated_time_total; // 合計の所要時間を保存（進捗表示のため）
       washing();
     }
     else if (elements[0] == "loading") { // Loadingの処理を実行
+      estimated_time_total = loading_time_total;
       loading();
     }
     else if (elements[0] == "collecting") { // Collectingの処理を実行
+      estimated_time_total = collecting_time_total;
       collecting();
     }
     else if (elements[0] == "all_phase") { // All phase（Washing→Loading→Collecting）の処理を実行
+      estimated_time_total = washing_time_total + loading_time_total + collecting_time_total;
       all_phase();
     }
 
@@ -240,12 +270,12 @@ void loop() {
 
 
     rx_sig_count_prev = rx_sig_count; // 信号を1回受信したら更新（これで次の命令を受信するまで動作を実行しない）
-    Serial.print("rx_sig_count = ");
-    Serial.print(rx_sig_count);
-    Serial.print(", rx_sig_count_prev = ");
-    Serial.print(rx_sig_count_prev);
-    Serial.print("\n--> Excecuted the received command\n");
-    Serial.println();
+    //    Serial.print("rx_sig_count = ");
+    //    Serial.print(rx_sig_count);
+    //    Serial.print(", rx_sig_count_prev = ");
+    //    Serial.print(rx_sig_count_prev);
+    //    Serial.print("\n--> Excecuted the received command\n");
+    //    Serial.println();
   }
 }
 /* ステッピングモータを駆動してカラムを前方に動かす関数 */
@@ -341,6 +371,7 @@ void pwm_pump_12ch(unsigned long running_time) {
 
 /* Phase1（Washing） */
 void washing() {
+  excecuted_process = "washing" ;
   step_front(50);
   step_back(200);
   /* 1-1 */
@@ -362,6 +393,7 @@ void washing() {
 
 /* Phase2（Loading） */
 void loading() {
+  excecuted_process = "loading" ;
   /* 2-1 */
   on_pump_dba(2);
   delay(t_21 * 1000); // Wait for 19 sec
@@ -404,6 +436,7 @@ void loading() {
 
 /* Phase3（Collecting） */
 void collecting() {
+  excecuted_process = "collecting" ;
   /* 3-1 */
   on_pump_dba(3);
   delay(t_31 * 1000); // Wait for 15 sec
